3980add0ec7c1da889eb5bf250f22731af6e0f43
주찬웅 <jcwleo@naver.com>
2017-11-04 01:30:39 +0900

    print ‘satisfiable’ or ‘unsatisfiable’ and truth assignment

--- DPLL_second.py
+++ DPLL_second.py
@@ -1,3 +1,18 @@
+from copy import deepcopy
+
+truth_assignment = []
+
+def remove_liter(cnf):
+    new_liters = list(''.join(cnf))
+    if '!' in new_liters:
+        new_liters.remove('!')
+    if '\n' in new_liters:
+        new_liters.remove('\n')
+    if ' ' in new_liters:
+        new_liters.remove(' ')
+    return new_liters
+
+
 def preproc(input_cnf):
     liters = list(set(input_cnf))
     if '!' in liters:
@@ -6,15 +21,15 @@ def preproc(input_cnf):
         liters.remove('\n')
     if ' ' in liters:
         liters.remove(' ')
-    liters = dict(zip(liters, [False] * len(liters)))
+    # liters = dict(zip(liters, [False] * len(liters)))
+    liters = list(liters)
     input_cnf = input_cnf.splitlines()
-    print('--------------------------------------------------')
-    print('CNF : ', input_cnf)
-    print('Liters : ', liters)
-    print('--------------------------------------------------')
+
     return liters, input_cnf
 
+
 def dpll(cnf, liters):
+    global truth_assignment
     # unit prop
     while True:
         if len(liters) == 1 and len(cnf) > 1:
@@ -27,54 +42,78 @@ def dpll(cnf, liters):
         unit_clause = None
         cnf = list(set(cnf))
         print('--------------------------------------------------')
-        print(cnf)
-        print(liters)
+        print('CNF : ', cnf)
+        print('Liters : ', liters)
 
         for i in range(len(cnf)):
             if ' ' not in cnf[i]:
                 unit_clause = cnf[i]
+                truth_assignment.append(unit_clause)
                 print('Unit : ', unit_clause)
                 del cnf[i]
                 break
 
         if unit_clause and '!' not in unit_clause:
-            del liters[unit_clause]
+            # liters.remove(unit_clause)
+            # liters = remove_liter(cnf)
             for j in range(len(cnf)):
                 if '!' + unit_clause in cnf[j]:
-                    cnf[j]= cnf[j].replace('!'+unit_clause,' ')
+                    cnf[j] = cnf[j].replace('!' + unit_clause, ' ')
                     cnf[j] = cnf[j].strip(' ')
                 elif unit_clause in cnf[j]:
                     delete_list.append(j)
 
             for index in sorted(delete_list, reverse=True):
                 del cnf[index]
+            liters = remove_liter(cnf)
 
         elif unit_clause and '!' in unit_clause:
-            del liters[unit_clause.replace('!','')]
+            # liters.remove(unit_clause.replace('!', ''))
+            # liters = remove_liter(cnf)
             for j in range(len(cnf)):
                 if unit_clause in cnf[j]:
                     delete_list.append(j)
-                elif unit_clause.replace('!','') in cnf[j]:
-                    cnf[j] = cnf[j].replace(unit_clause.replace('!','') , ' ')
+                elif unit_clause.replace('!', '') in cnf[j]:
+                    cnf[j] = cnf[j].replace(unit_clause.replace('!', ''), ' ')
                     cnf[j] = cnf[j].strip(' ')
             for index in sorted(delete_list, reverse=True):
                 del cnf[index]
+            liters = remove_liter(cnf)
 
         else:
             break
 
-    ##
+    # checking clause
     if cnf == []:
-        return 'Satis'
-    elif len(liters) == 1 and len(cnf) > 1:
-        return 'UnSatis'
+        return True
+
+    if len(liters) == 1 and len(cnf) > 1:
+        return False
+
+    # case-splitting
+    if dpll(cnf + [liters[0]], deepcopy(liters)) or dpll(cnf + ['!' + liters[0]], deepcopy(liters)):
+        return True
+    else:
+        return False
 
 
 def main():
+    global truth_assignment
     input_cnf = open('input.txt', 'r').read()
     liters, cnf = preproc(input_cnf)
-    print(dpll(cnf,liters))
-    pass
+    if dpll(cnf, liters):
+        print('--------------------------------------------------')
+        print('Result : Satisfiable')
+        for i in truth_assignment:
+            if '!' in i:
+                print(i.replace('!',''),' : False', end=' | ')
+            else:
+                print(i,' : True', end=' | ')
+    else:
+        print('--------------------------------------------------')
+        print('Result : UnSatisfiable')
+        print(truth_assignment)
+
 
 if __name__ == '__main__':
     main()
